96e1bafe7fd6b1caa274ea2a0eb13323fc7b6cab
ayeonul <kde013434@gmail.com>
2024-06-18 23:15:15 +0900

    add footer, update intro page

--- src/App.vue
+++ src/App.vue
@@ -1,24 +1,30 @@
 <template>
-  <div id="app">
-    <Header v-show="isRoute"/>
-    <router-view :class="{'route-view': isRoute}"/>
-  </div>
+    <div id="app">
+        <div class="content-container">
+            <Header v-show="isRoute" />
+            <router-view :class="{ 'route-view': isRoute }" />
+        </div>
+        <Footer v-show="isRoute" />
+    </div>
 </template>
 
 <script>
 import Header from './components/Header.vue';
+import Footer from './components/Footer.vue';
+
 export default {
-  components: {
-    Header
-  },
-  computed: {
-    isRoute() {
-      return this.$route.path !== '/';
+    components: {
+        Header,
+        Footer,
     },
-  }
+    computed: {
+        isRoute() {
+            return this.$route.path !== '/';
+        },
+    },
 };
 </script>
 
 <style>
-@import url("@/assets/styles/App.css");
+@import url('@/assets/styles/App.css');
 </style>
