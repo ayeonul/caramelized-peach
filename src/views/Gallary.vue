f5ee01a6b96957edc9703db9130f57fe0a7aa210
ayeonul <kde013434@gmail.com>
2024-06-24 21:36:19 +0900

    add nsfw filter

--- src/views/Gallary.vue
+++ src/views/Gallary.vue
@@ -1,7 +1,17 @@
 <template>
     <div>
+        <div class="gallary-info">
+            아래 이미지는 모두 커미션 혹은 지인들에게 선물받은 그림으로, 모든
+            저작권은 그림의 작가님께 있습니다.
+        </div>
         <div class="carousel-section">
             <GallaryCarousel :images="images" />
+        </div>
+        <div class="toggle-section">
+            <input type="checkbox" id="NSFWToggle" v-model="showNSFW" />
+            <label for="NSFWToggle" class="checkbox-label"
+                >후방주의 그림 표시하기</label
+            >
         </div>
     </div>
 </template>
@@ -10,38 +20,60 @@
 import GallaryCarousel from '@/components/GallaryCarousel.vue';
 
 function importAll(r) {
-  return r.keys().map(r);
+    return r.keys().map(r);
 }
 
 const imageFiles = importAll(
-  require.context('@/assets/imgs/gallary', false, /\.(png|jpe?g|svg)$/)
+    require.context('@/assets/imgs/gallary', false, /\.(png|jpe?g|svg)$/)
 );
 
 const images = imageFiles.reduce((acc, img) => {
-  const fileName = img.split('/').pop().split('.')[0]; // Extract file name without extension
-  acc[fileName] = img;
-  return acc;
+    const fileName = img.split('/').pop().split('.')[0]; // Extract file name without extension
+    acc[fileName] = img;
+    return acc;
 }, {});
 
 export default {
     components: {
         GallaryCarousel,
     },
+    computed: {
+        showNSFW: {
+            get() {
+                return this.$store.state.showNSFW;
+            },
+            set(value) {
+                this.$store.state.showNSFW = value;
+            },
+        },
+    },
     data() {
         return {
             images,
         };
     },
-    methods:{
-    }
+    methods: {},
 };
 </script>
 
 <style scoped>
+.gallary-info {
+    font-family: 'pretendard';
+    text-align: center;
+    font-size: 0.9rem;
+    color: #888;
+}
+.toggle-section {
+    font-family: 'pretendard';
+    font-size: 0.8rem;
+    font-weight: 500;
+    color: #888;
+    align-content: center;
+    text-align: center;
+}
+
 .carousel-section {
-    margin: 0 auto;
-    width: 80%;
     height: 50vh;
-    padding: 2rem;
+    margin: 1rem 0;
 }
 </style>
