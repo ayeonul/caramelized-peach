67587b963fe1364524a2fff4a2b56d24bd523b06
ayeonul <kde013434@gmail.com>
2024-09-17 23:33:18 +0900

    update new header

--- src/components/Header.vue
+++ src/components/Header.vue
@@ -1,45 +1,67 @@
 <template>
-    <div class="header">
-        <router-link to="/" class="logo-section">
-            <div class="logo-item">Caramelized</div>
-            <div class="logo-item">Peach</div>
-        </router-link>
-        <div class="nav">
-            <router-link
-                to="/introduce"
-                class="nav-item"
-                :class="isRoute('/introduce')"
-                >Introduce</router-link
-            >
-            <router-link
-                to="/gallary"
-                class="nav-item"
-                :class="isRoute('/gallary')"
-                >Gallary</router-link
-            >
-            <router-link
-                to="/archive"
-                class="nav-item"
-                :class="isRoute('/archive')"
-                >Archive</router-link
-            >
-        </div>
+  <div class="header">
+    <router-link to="/" class="logo-section">
+      <!-- <div class="logo-item">Caramelized</div>
+            <div class="logo-item">Peach</div> -->
+      <div class="logo-item">THE LIVERFORD TIMES</div>
+    </router-link>
+    <div class="info-section">
+      {{ getTodayDate() }}
     </div>
+    <div class="nav">
+      <router-link
+        to="/introduce"
+        class="nav-item"
+        :class="isRoute('/introduce')"
+        >Introduce</router-link
+      >
+      <router-link to="/gallary" class="nav-item" :class="isRoute('/gallary')"
+        >Gallary</router-link
+      >
+      <router-link to="/archive" class="nav-item" :class="isRoute('/archive')"
+        >Archive</router-link
+      >
+    </div>
+  </div>
 </template>
 
 <script>
 export default {
-    methods: {
-        isRoute(targetRoute) {
-            if (this.$route.path === targetRoute) {
-                return 'is-active';
-            }
-            return '';
-        },
+  methods: {
+    isRoute(targetRoute) {
+      if (this.$route.path === targetRoute) {
+        return "is-active";
+      }
+      return "";
     },
+    getTodayDate() {
+      const today = new Date();
+      const optionsDate  = {
+        timeZone: 'Asia/Seoul', // KST 타임존 설정
+        year: 'numeric',
+        month: '2-digit',
+        day: '2-digit'
+      };
+
+      const optionsWeekday = {
+        timeZone: 'Asia/Seoul',
+        weekday: 'long'
+      };
+
+      // 날짜 포맷 (년. 월. 일 형식으로 가져오기)
+      const formattedDate = new Intl.DateTimeFormat('ko-KR', optionsDate).format(today);
+      const [year, month, day] = formattedDate.split('. ').map(part => part.replace('.', ''));
+
+      // 요일 포맷 (영어로 요일 가져오기)
+      const weekday = new Intl.DateTimeFormat('en-US', optionsWeekday).format(today);
+
+      // "DD-MM-YYYY (요일)" 형식으로 반환
+      return `${weekday} ${day}-${month}-${year} `;
+    }
+  },
 };
 </script>
 
 <style>
-@import url('@/assets/styles/Header.css');
+@import url("@/assets/styles/Header.css");
 </style>
