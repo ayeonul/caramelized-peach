f4f2609dd2d939b528076cefae6ebfc63e4451da
ayeonul <kde013434@gmail.com>
2024-09-18 23:35:05 +0900

    add background paper & transition

--- src/App.vue
+++ src/App.vue
@@ -3,19 +3,38 @@
     <div class="wave-gradient"></div>
     <div class="dark-veil"></div>
 
-    <div
-      :class="{ 'content-container': isRoute }"
-      ref="contentContainer"
-    >
-      <div class="papers" v-show="isRoute">
-        <div class="stacked-paper paper-1"></div>
-        <div class="stacked-paper paper-2"></div>
+    <transition name="fade" mode="out-in">
+      <div v-if="isNotRoute" class="fade" key="not-route-content">
+        <router-view />
       </div>
-      <Header ref="header" v-show="isRoute" />
-      <router-view :class="{ 'route-view no-scrollbar': isRoute }" />
-      <div class="spacer" v-show="isRoute "></div>
-      <!-- <Footer ref="footer" v-show="isRoute" /> -->
+    </transition>
+
+    <transition name="slide-bounce" mode="out-in">
+      <div
+        v-if="!isNotRoute"
+        class="slide content-container"
+        key="route-content"
+      >
+        <div class="papers">
+          <div class="stacked-paper paper-1"></div>
+          <div class="stacked-paper paper-2"></div>
+        </div>
+        <Header ref="header" />
+        <router-view class="route-view no-scrollbar" />
+        <div class="spacer"></div>
+      </div>
+    </transition>
+
+    <transition name="paper-slide" mode="out-in">
+    <div v-show="!isNotRoute" class="backgroud-papers">
+      <img src="@/assets/imgs/background/신문1.png" class="bg-paper bg-paper-1"/>
+      <img src="@/assets/imgs/background/신문2.png" class="bg-paper bg-paper-2"/>
+      <img src="@/assets/imgs/background/신문3.png" class="bg-paper bg-paper-3"/>
+      <img src="@/assets/imgs/background/신문4.png" class="bg-paper bg-paper-4"/>
+      <img src="@/assets/imgs/background/신문5.png" class="bg-paper bg-paper-5"/>
+      <img src="@/assets/imgs/background/신문6.png" class="bg-paper bg-paper-6"/>
     </div>
+    </transition>
   </div>
 </template>
 
@@ -29,12 +48,9 @@ export default {
     Footer,
   },
   computed: {
-    isRoute() {
-      return this.$route.path !== "/";
+    isNotRoute() {
+      return this.$route.path == "/";
     },
-    isNotIntroduce(){
-      return this.$route.path !== "/introduce";
-    }
   },
   data() {
     return {};
@@ -54,4 +70,77 @@ export default {
 <style>
 @import url("@/assets/styles/pretendard.css");
 @import url("@/assets/styles/App.css");
+</style>
+
+
+<style scoped>
+/* transition */
+.fade-enter-active,
+.fade-leave-active {
+  transition: opacity 0.2s ease;
+}
+
+.fade-enter {
+  opacity: 1; /* 나타날 때는 애니메이션 없이 바로 등장 */
+}
+
+.fade-leave-to {
+  opacity: 0; /* 사라질 때는 서서히 투명해짐 */
+}
+
+@keyframes slideInBounce {
+  0% {
+    transform: translateY(100%);
+  }
+  70% {
+    transform: translateY(-8%); /* 살짝 위로 이동 */
+  }
+  100% {
+    transform: translateY(0); /* 제자리로 돌아옴 */
+  }
+}
+
+@keyframes slideOutBounce {
+  0% {
+    transform: translateY(0);
+  }
+  30% {
+    transform: translateY(-8%); /* 위로 살짝 갔다가 */
+  }
+  100% {
+    transform: translateY(100%); /* 아래로 사라짐 */
+  }
+}
+
+
+
+/* slide 트랜지션에 적용되는 바운스 효과 */
+
+.slide-bounce-enter-active, .slide-bounce-leave-active{
+  transition-delay: 0.2s;
+}
+.slide-bounce-enter-active {
+  animation: slideInBounce 0.3s ease-out;
+}
+
+.slide-bounce-leave-active {
+  animation: slideOutBounce 0.3s ease-in;
+}
+
+
+.paper-slide-enter-active, .paper-slide-leave-active {
+  transition: transform 0.2s ease;
+}
+
+.paper-slide-enter {
+  transform: translateY(100%); /* 아래에서 시작 */
+}
+
+.paper-slide-enter-to {
+  transform: translateY(0); /* 위로 올라옴 */
+}
+
+.paper-slide-leave-to {
+  transform: translateY(100%); /* 아래로 내려감 */
+}
 </style>
